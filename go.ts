e6d921ecc50c24ffdba1232e4467750e5b44ef5f
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-02 13:00:44 +0100

    Added quine test.

--- go.ts
+++ go.ts
@@ -5,7 +5,7 @@ import fs = require("fs");
 const interpreter = Environment.createStandard().createInterpreter();
 let output = "";
 interpreter.env.def("print", (message: string) => output += message);
-const source = fs.readFileSync("quine.hodor", "utf8");
+const source = fs.readFileSync("test/lang/quine.hodor", "utf8");
 interpreter.execute(source);
 console.log("Is quine?");
 console.log(output === source);
--- test/lang/languageTests.ts
+++ test/lang/languageTests.ts
@@ -11,10 +11,32 @@ test.todo("Quine", null);
 
 test.todo("Check out pify", null);
 
-glob.sync(packageDir + "/test/**/*.hodor").forEach(filename => {
-    test("Hodor script should not throw for file " + filename, t => {
-        const code = fs.readFileSync(filename, "utf8");
-        const interpreter = new Environment().withConsoleLogger().createInterpreter();
-        t.notThrows(() => interpreter.execute(code));
+function executeAndGetOutput(code: string) {
+    let output = "";
+    const interpreter = Environment.createStandard().createInterpreter();
+    interpreter.env.def("print", (message: string) => output += message);
+    interpreter.execute(code);
+
+    return output;
+}
+
+const specificTests: { [filename: string]: (filepath: string) => void } = {
+    ["quine.hodor"]: filepath => {
+        test(`${filepath} should be a quine`, t => {
+            const code = fs.readFileSync(filepath, "utf8");
+            t.is(executeAndGetOutput(code), code);
+        });
+    }
+}
+
+glob.sync(packageDir + "/test/**/*.hodor").forEach(filepath => {
+    test("Hodor script should not throw for file " + filepath, t => {
+        t.notThrows(() => executeAndGetOutput(fs.readFileSync(filepath, "utf8")));
     });
+
+    const filename = filepath.split("/").slice(-1)[0];
+    const specificTest = specificTests[filename];
+    specificTest && specificTest(filepath);
 });
+
+
