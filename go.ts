90f1f4fc49eb684f3a11fd6c63cc7c992287da5f
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-02 16:34:01 +0100

    Working quine, with almost all variables hodorised.

--- go.ts
+++ go.ts
@@ -8,23 +8,34 @@ const interpreter = Environment.createStandard().createInterpreter();
 function buildQuine() {
     const evalableLines = [
         ``,
-        `s = fromCharCode(64) + fromCharCode(34);`,
-        `e = fromCharCode(34) + fromCharCode(64);`,
-        `print("c = "+s+hhodor(c)+e+";eval(c);")`,
+        `@s = fromCharCode(64) + fromCharCode(34);`,
+        `@e = fromCharCode(34) + fromCharCode(64);`,
+        `print("@c = "+@s+hhodor(@c)+@e+";eval(@c);")`,
         ``
     ];
 
+    function hodoriseQuotes(code: string) {
+        return code.replace(/"([^"]+)"/g, (match, group1) => `"` + Transpiler.Hodor(group1) + `"`);
+    }
+    function hodoriseVariables(code: string) {
+        return code.replace(/@(\w+?)\b/g, (match, group1) => `'` + Transpiler.Hodor(group1) + `'`);
+    }
+
     /// the string literal will be evaled, so literals inside it need to be hodorised
     const substringsHodorisedLines = evalableLines.map(line => {
-        const hodorisedQuotes = line.replace(/"([^"]+)"/g, (match, group1) => `"` + Transpiler.Hodor(group1) + `"`);
-        return hodorisedQuotes;
+        line = hodoriseVariables(line);
+        line = hodoriseQuotes(line);
+        return line;
+        // const hodorisedQuotes = hodoriseQuotes(line);
+        // const hodorisedVariables = hodoriseVariables(hodorisedQuotes);
+        // return hodorisedVariables;
     });
 
     const evalableString = substringsHodorisedLines.join(`\r\n`);
 
     const hodorisedEvalableString = Transpiler.Hodor(evalableString);
 
-    const quine = `c = @"` + hodorisedEvalableString + `"@;eval(c);`;
+    const quine = hodoriseVariables(`@c = @"`) + hodorisedEvalableString + hodoriseVariables(`"@;eval(@c);`);
 
     return quine;
 };
@@ -33,14 +44,17 @@ let source = buildQuine();
 let output = "";
 interpreter.env.def("print", (message: string) => output += message);
 
-//source = `'HODOR!' = "Hodor."; print('HODOR!');`; 
+// source = `'HODOR.' = "Hodor."; print(E);`; 
 
 interpreter.execute(source);
 
 console.log("Is quine?");
 console.log(output === source);
 
-if (output !== source) {
+if (output === source){
+    fs.writeFileSync("foo.hodor", source, { encoding: "utf8" });
+}
+else {
     console.log("source########\r\n");
     console.log(source);
     console.log("output########\r\n");
