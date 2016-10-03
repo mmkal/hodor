d141d9b0a3dbbb5e906c3a7133ac1df06e92a025
Misha Kaletsky <misha.kaletsky@gmail.com>
2016-10-03 14:29:25 +0100

    Added hello world test.

--- test/lang/languageTests.ts
+++ test/lang/languageTests.ts
@@ -42,53 +42,24 @@ availablePathEndings.forEach(path => {
 });
 
 function buildQuine() {
-    const evalableLines = [
-        ``,
-        `$s = $fromCharCode(64) + $fromCharCode(34);`,
-        `$e = $fromCharCode(34) + $fromCharCode(64);`,
-        `$print("$c = "+$s+$hodor($c)+$e+";$eval($c);")`,
-        ``
-    ];
-
-    const n00b = `
+    const wylis = `
 $s = $fromCharCode(64) + $fromCharCode(34);
 $e = $fromCharCode(34) + $fromCharCode(64);
 $print("$c = "+$s+$hodor($c)+$e+";$eval($c);")
 `;
-    const evalable = Hodor.n00b(n00b);
-    const literal = Hodor.Hodor(evalable);
+    const hodor = Hodor.n00b(wylis);
+    const literal = Hodor.Hodor(hodor);
 
     const quine = Hodor.n00b(`$c = @"`) + literal + Hodor.n00b(`"@;$eval($c);\r\n`);
 
-    // let hodor = Hodor.n00b(evalableLines.join("\r\n"));
-
-    // function hodoriseQuotes(code: string) {
-    //     return code.replace(/"([^"]+)"/g, (match, group1) => `"` + Hodor.Hodor(group1) + `"`);
-    // }
-    // function hodoriseVariables(code: string) {
-    //     return code.replace(/\$(\w+?)\b/g, (match, group1) => `'` + Hodor.Hodor(group1) + `'`);
-    // }
-
-    // /// the string literal will be evaled, so literals inside it need to be hodorised
-    // const substringsHodorisedLines = evalableLines.map(line => {
-    //     line = hodoriseVariables(line);
-    //     line = hodoriseQuotes(line);
-    //     return line;
-    // });
-
-    // const evalableString = substringsHodorisedLines.join(`\r\n`);
-
-    // const b1 = hodor === evalableString;
-    // hodor = Hodor.n00b(`$c = @"`) + Hodor.Hodor(hodor) + Hodor.n00b(`"@;$eval($c);\r\n`);
-
-    // const hodorisedEvalableString = Hodor.Hodor(evalableString);
-
-    // const quine = hodoriseVariables(`$c = @"`) + hodorisedEvalableString + hodoriseVariables(`"@;$eval($c);\r\n`);
-
-    // console.log(hodor === quine);
-
     return quine;
 };
+
+test("Print hello world", t => {
+    const code = Hodor.n00b(`$print("Hello, world!");`);
+    const output = executeAndGetOutput(code);
+    t.is(output, "Hello, world!\r\n");
+});
 
 test(`${buildQuine.name} should build a quine`, t => {
     const quine = buildQuine();
