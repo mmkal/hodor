174877429efec858f4976c2313ccdd6a71a2db08
Misha Kaletsky <mmkal@users.noreply.github.com>
2016-10-04 15:59:52 +0100

    Added tests for hodoring and wylising twice.

--- test/lang/transpilerTests.ts
+++ test/lang/transpilerTests.ts
@@ -2,7 +2,7 @@ import Hodor from "../../lang/Hodor";
 import {test,packageDir} from "../_ava-shim";
 import fs = require("fs");
 
-[
+const sampleStrings = [
     "e",
     "et",
     "e t",
@@ -15,10 +15,20 @@ import fs = require("fs");
     "Hodor?",
     "// It rubs the lotion on its skin, or else it gets the hose again.",
     fs.readFileSync(packageDir + "/package.json", "utf8")
-].forEach(original => {
+];
+
+sampleStrings.forEach(original => {
     test("Hodor then Wylis returns original: " + original.split("\n")[0], t => {
         const hodor = Hodor.Hodor(original);
         const wylis = Hodor.Wylis(hodor);
+        t.is(wylis, original);
+    });
+});
+
+sampleStrings.forEach(original => {
+    test("Hodor twice then Wylis twice returns original: " + original.split("\n")[0], t => {
+        const hodor = Hodor.Hodor(Hodor.Hodor(original));
+        const wylis = Hodor.Wylis(Hodor.Wylis(hodor));
         t.is(wylis, original);
     });
 });
