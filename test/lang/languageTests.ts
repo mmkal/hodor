b0b5411f2ecc322d367730a51a52b68b7b6f9a1f
Misha Kaletsky <mmkal@users.noreply.github.com>
2016-10-04 13:42:49 +0100

    Removed filesystem tests.

--- test/lang/languageTests.ts
+++ test/lang/languageTests.ts
@@ -1,8 +1,6 @@
 import {test, packageDir} from "../_ava-shim";
-import fs = require("fs");
 import Environment from "../../lang/Environment";
 import Samples from "../../lang/Samples";
-import glob = require("glob");
 
 function executeAndGetOutput(code: string) {
     const output = new Array<string>();
@@ -13,34 +11,6 @@ function executeAndGetOutput(code: string) {
 
     return output.join("\r\n");
 }
-
-const hodorTests: { [pathEnding: string]: (filepath: string, code: string) => void } = {
-    ["quine.hodor"]: (filepath, code) => {
-        test(`${filepath} should be a quine`, t => t.is(executeAndGetOutput(code), code));
-    },
-    ["hello-world.hodor"]: (filepath, code) => {
-        test(`${filepath} should output hello world`, t => t.is(executeAndGetOutput(code), "Hello, World!"));
-    },
-    [""]: (filepath, code) => {
-        test(filepath + " should not throw", t => t.notThrows(() => executeAndGetOutput(code)));
-    }
-}
-
-const matchedPathEndings = new Set<string>();
-const availablePathEndings = Object.keys(hodorTests); 
-glob.sync(packageDir + "/**/*.hodor").forEach(filepath => {
-    const code = fs.readFileSync(filepath, "utf8");
-    availablePathEndings
-        .filter(pathEnding => filepath.endsWith(pathEnding))
-        .forEach(pathEnding => {
-            hodorTests[pathEnding](filepath, code);
-            matchedPathEndings.add(pathEnding);
-        });
-});
-
-availablePathEndings.forEach(path => {
-    test(`Path ending "${path}" should have matched at some point`, t => t.true(matchedPathEndings.has(path)));
-});
 
 test(Samples.helloWorld.name + " sample", t => {
     t.is(executeAndGetOutput(Samples.helloWorld()), "Hello, World!");
