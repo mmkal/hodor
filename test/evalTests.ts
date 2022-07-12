efb81cbe39677db923def0736cbb51b13d1d3db2
mmkal <misha.kaletsky@gmail.com>
2022-07-12 18:39:53 -0400

    add evalTests

---
+++ test/evalTests.ts
@@ -0,0 +1,6 @@
+import * as hodor from "../hodor/Hodor";
+
+test("adding", () => expect(hodor.hodor("1 + 2")).toBe(3));
+test("bodMas", () => expect(hodor.hodor("1 + 2 * 3")).toBe(7));
+test("Bodmas", () => expect(hodor.hodor("(1 + 2) * 3")).toBe(9));
+test("BoDmas", () => expect(hodor.hodor("1 + 2 * 3 / 4")).toBe(2.5));
