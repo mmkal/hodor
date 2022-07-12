718437e5a351fc0f16ce840493b0598128c1d08f
mmkal <misha.kaletsky@gmail.com>
2022-07-12 18:32:11 -0400

    remove all dependencies, expose eval function

--- cli.ts
+++ cli.ts
@@ -3,37 +3,41 @@ import fs = require("fs");
 import Environment from "./hodor/Environment";
 import repl = require("repl");
 import Hodor from "./hodor/Hodor";
-import chalk from 'chalk'
+import chalk = require("./hodor/colors");
 
 const hodorArgv = process.argv.slice(2);
 const __hodorfile = hodorArgv[0];
 
 if (__hodorfile) {
-    const code = fs.readFileSync(__hodorfile, "utf8");
-    const interpreter = Environment.createStandard().withKVs({__hodorfile}).createInterpreter();
-    interpreter.execute(code);
-}
-else {
-    const interpreter = Environment.createStandard().createInterpreter();
-    repl.start({
-        prompt: chalk.green("Hodor? "),
-        eval: (cmd: string, _context: any, _filename: string, callback: Function) => {
-            const input = cmd.trim();
-            try {
-                const value = interpreter.execute(input);
-                const toPrint = value ? JSON.stringify(value) : value;
-                typeof toPrint !== "undefined" && console.log(
-                    chalk.yellow("Hodor! ") + chalk.grey(toPrint));
-            }
-            catch (evalError) {
-                try {
-                    console.log(chalk.blue("Hodor! ") + Hodor.Wylis(input));
-                }
-                catch(wylisError) {
-                    console.log(chalk.red("Hodor! ") + Hodor.Hodor(input));
-                }
-            }
-            callback();
+  const code = fs.readFileSync(__hodorfile, "utf8");
+  const interpreter = Environment.createStandard()
+    .withKVs({ __hodorfile })
+    .createInterpreter();
+  interpreter.execute(code);
+} else {
+  const interpreter = Environment.createStandard().createInterpreter();
+  repl.start({
+    prompt: chalk.green("Hodor? "),
+    eval: (
+      cmd: string,
+      _context: any,
+      _filename: string,
+      callback: Function
+    ) => {
+      const input = cmd.trim();
+      try {
+        const value = interpreter.execute(input);
+        const toPrint = value ? JSON.stringify(value) : value;
+        typeof toPrint !== "undefined" &&
+          console.log(chalk.yellow("Hodor! ") + chalk.grey(toPrint));
+      } catch (evalError) {
+        try {
+          console.log(chalk.blue("Hodor! ") + Hodor.Wylis(input));
+        } catch (wylisError) {
+          console.log(chalk.red("Hodor! ") + Hodor.Hodor(input));
         }
-    });
+      }
+      callback();
+    },
+  });
 }
--- hodor/Hodor.ts
+++ hodor/Hodor.ts
@@ -1,188 +1,198 @@
-export module Hodor {
-    enum MorseUnit {
-        Dot,
-        Dash
-    }
+import Environment from "./Environment";
 
-    const _hodors = BuildHodors();
-    const _nodors = BuildNodors();
-    const _nodorKeys = Object.keys(_nodors).sort((k1, k2) => k2.length - k1.length);
+module Hodor {
+  enum MorseUnit {
+    Dot,
+    Dash,
+  }
 
-    const _beforeHodor = "Ho-dor? ";
-    const _afterHodor = " Ho-dor!";
+  const _hodors = BuildHodors();
+  const _nodors = BuildNodors();
+  const _nodorKeys = Object.keys(_nodors).sort(
+    (k1, k2) => k2.length - k1.length
+  );
 
-    export function Hodor(wylis: string) {
-        const hodors = new Array<string>();
-        let lastWasHodor = false;
-        let lastHodor: string | null = null;
+  const _beforeHodor = "Ho-dor? ";
+  const _afterHodor = " Ho-dor!";
 
-        [...wylis].forEach(ch => {
-            const hodor = _hodors[ch];
-            if (hodor) {
-                if (lastWasHodor) {
-                    hodors.push(" ");
-                }
-                else if (lastHodor === " ") {
-                    hodors.push(_beforeHodor);
-                }
-                hodors.push(hodor);
-            }
-            else {
-                if (lastWasHodor) {
-                    hodors.push(_afterHodor);
-                }
-                hodors.push(ch);
-            }
+  export function Hodor(wylis: string) {
+    const hodors = new Array<string>();
+    let lastWasHodor = false;
+    let lastHodor: string | null = null;
 
-            lastWasHodor = !!hodor;
-            lastHodor = ch;
-        });
-
-        return hodors.join("");
-    }
-
-    function escapeRegExp(str: string) {
-       return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-    }
-
-    export function Wylis(hodor: string) {
-        const splitRegex = `(${escapeRegExp(_beforeHodor)})|(${escapeRegExp(_afterHodor)})`;
-        const wylis = hodor
-            .split(new RegExp(splitRegex))
-            .filter(hodorToken => typeof hodorToken !== "undefined" && hodorToken !== _beforeHodor && hodorToken !== _afterHodor)
-            .map(hodorToken => {
-                const replacements = getWylisReplacements(hodorToken);
-
-                Object.keys(replacements)
-                    .map(k => JSON.parse(k))
-                    .sort((left, right) => right[0] - left[0])
-                    .forEach(tuple => {
-                        const before = hodorToken.substring(0, tuple[0]);
-                        const after = hodorToken.substring(tuple[1]);
-
-                        hodorToken = before + replacements[JSON.stringify(tuple)] + after;
-                    });
-
-                return hodorToken;
-            })
-            .join("");
-
-        if (Hodor(wylis) !== hodor) {
-            throw new Error(`String ${hodor} is not correctly hodorised.`);
+    [...wylis].forEach((ch) => {
+      const hodor = _hodors[ch];
+      if (hodor) {
+        if (lastWasHodor) {
+          hodors.push(" ");
+        } else if (lastHodor === " ") {
+          hodors.push(_beforeHodor);
         }
-        return wylis;
-    }
-
-    function getWylisReplacements(hodorToken: string) {
-        const replacements: { [key: string]: string } = {};
-        _nodorKeys
-            .forEach(nodor => {
-                const nodorRegex = new RegExp(escapeRegExp(nodor), "gm");
-                const indexes = new Array<number>(); 
-                
-                let match: RegExpExecArray | null;
-                while ((match = nodorRegex.exec(hodorToken)) !== null) {
-                    indexes.push(match.index);
-                }
-
-                indexes.forEach(index => {
-                    const overlapping = Object
-                        .keys(replacements)
-                        .map(k => JSON.parse(k))
-                        .some((tuple: number[]) => index >= tuple[0] && index < tuple[1]);
-                    
-                    if (overlapping) return;
-
-                    const key = JSON.stringify([index, index + nodor.length]);
-                    replacements[key] = _nodors[nodor];
-                });
-            });
-
-        return replacements;
-    }
-
-    function ParseMorseUnit(symbol: string) {
-        if (symbol === ".") {
-            return MorseUnit.Dot;
+        hodors.push(hodor);
+      } else {
+        if (lastWasHodor) {
+          hodors.push(_afterHodor);
         }
-        else if (symbol === "-") {
-            return MorseUnit.Dash;
+        hodors.push(ch);
+      }
+
+      lastWasHodor = !!hodor;
+      lastHodor = ch;
+    });
+
+    return hodors.join("");
+  }
+
+  function escapeRegExp(str: string) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  }
+
+  export function Wylis(hodor: string) {
+    const splitRegex = `(${escapeRegExp(_beforeHodor)})|(${escapeRegExp(
+      _afterHodor
+    )})`;
+    const wylis = hodor
+      .split(new RegExp(splitRegex))
+      .filter(
+        (hodorToken) =>
+          typeof hodorToken !== "undefined" &&
+          hodorToken !== _beforeHodor &&
+          hodorToken !== _afterHodor
+      )
+      .map((hodorToken) => {
+        const replacements = getWylisReplacements(hodorToken);
+
+        Object.keys(replacements)
+          .map((k) => JSON.parse(k))
+          .sort((left, right) => right[0] - left[0])
+          .forEach((tuple) => {
+            const before = hodorToken.substring(0, tuple[0]);
+            const after = hodorToken.substring(tuple[1]);
+
+            hodorToken = before + replacements[JSON.stringify(tuple)] + after;
+          });
+
+        return hodorToken;
+      })
+      .join("");
+
+    if (Hodor(wylis) !== hodor) {
+      throw new Error(`String ${hodor} is not correctly hodorised.`);
+    }
+    return wylis;
+  }
+
+  function getWylisReplacements(hodorToken: string) {
+    const replacements: { [key: string]: string } = {};
+    _nodorKeys.forEach((nodor) => {
+      const nodorRegex = new RegExp(escapeRegExp(nodor), "gm");
+      const indexes = new Array<number>();
+
+      let match: RegExpExecArray | null;
+      while ((match = nodorRegex.exec(hodorToken)) !== null) {
+        indexes.push(match.index);
+      }
+
+      indexes.forEach((index) => {
+        const overlapping = Object.keys(replacements)
+          .map((k) => JSON.parse(k))
+          .some((tuple: number[]) => index >= tuple[0] && index < tuple[1]);
+
+        if (overlapping) return;
+
+        const key = JSON.stringify([index, index + nodor.length]);
+        replacements[key] = _nodors[nodor];
+      });
+    });
+
+    return replacements;
+  }
+
+  function ParseMorseUnit(symbol: string) {
+    if (symbol === ".") {
+      return MorseUnit.Dot;
+    } else if (symbol === "-") {
+      return MorseUnit.Dash;
+    } else {
+      /* istanbul ignore next */
+      throw new Error(symbol + " cannot be parsed as a morse unit.");
+    }
+  }
+
+  function HodoriseMorseUnits(units: MorseUnit[]) {
+    return units
+      .map((unit, index) => {
+        const isDot = unit === MorseUnit.Dot;
+        if (index < units.length - 1) {
+          return isDot ? "Hodor" : "Hodor,";
         }
-        else {
-            /* istanbul ignore next */ 
-            throw new Error(symbol + " cannot be parsed as a morse unit.");
+        // Now deal with the last unit:
+        else if (isDot) {
+          return "Hodor.";
+        } else if (units.length % 2 === 0) {
+          return "Hodor?";
+        } else {
+          return "Hodor!";
         }
-    }
+      })
+      .join(" ");
+  }
 
-    function HodoriseMorseUnits(units: MorseUnit[]) {
-        return units.map((unit, index) => {
-            const isDot = unit === MorseUnit.Dot;
-            if (index < units.length - 1) {
-                return isDot ? "Hodor" : "Hodor,";
-            }
-            // Now deal with the last unit:
-            else if (isDot) {
-                return "Hodor.";
-            }
-            else if (units.length % 2 === 0) {
-                return "Hodor?";
-            }
-            else {
-                return "Hodor!";
-            }
-        }).join(" ");
-    }
+  function BuildHodors() {
+    const hodors: { [key: string]: string } = {
+      a: ".-",
+      b: "-...",
+      c: "-.-.",
+      d: "-..",
+      e: ".",
+      f: "..-.",
+      g: "--.",
+      h: "....",
+      i: "..",
+      j: ".---",
+      k: "-.-",
+      l: ".-..",
+      m: "--",
+      n: "-.",
+      o: "---",
+      p: ".--.",
+      q: "--.-",
+      r: ".-.",
+      s: "...",
+      t: "-",
+      u: "..-",
+      v: "...-",
+      w: ".--",
+      x: "-..-",
+      y: "-.--",
+      z: "--..",
+    };
+    Object.keys(hodors).forEach((k) => {
+      const morse = hodors[k];
+      const units = [...morse].map(ParseMorseUnit);
+      const hodor = HodoriseMorseUnits(units);
+      hodors[k] = hodor;
+      hodors[k.toUpperCase()] = hodor.toUpperCase();
+    });
 
-    function BuildHodors() {
-        const hodors: { [key: string]: string } = {
-            a: ".-",
-            b: "-...",
-            c: "-.-.",
-            d: "-..",
-            e: ".",
-            f: "..-.",
-            g: "--.",
-            h: "....",
-            i: "..",
-            j: ".---",
-            k: "-.-",
-            l: ".-..",
-            m: "--",
-            n: "-.",
-            o: "---",
-            p: ".--.",
-            q: "--.-",
-            r: ".-.",
-            s: "...",
-            t: "-",
-            u: "..-",
-            v: "...-",
-            w: ".--",
-            x: "-..-",
-            y: "-.--",
-            z: "--.."
-        };
-        Object.keys(hodors).forEach(k => {
-            const morse = hodors[k];
-            const units = [...morse].map(ParseMorseUnit);
-            const hodor = HodoriseMorseUnits(units);
-            hodors[k] = hodor;
-            hodors[k.toUpperCase()] = hodor.toUpperCase();
-        });
+    return hodors;
+  }
 
-        return hodors;
-    }
-
-    function BuildNodors() {
-        const nodors: { [key: string]: string } = {};
-        Object.keys(_hodors).forEach(k => {
-            const key = _hodors[k];
-            const value = k;
-            nodors[key] = value;
-            nodors[" " + key] = value;
-        });
-        return nodors;
-    }
+  function BuildNodors() {
+    const nodors: { [key: string]: string } = {};
+    Object.keys(_hodors).forEach((k) => {
+      const key = _hodors[k];
+      const value = k;
+      nodors[key] = value;
+      nodors[" " + key] = value;
+    });
+    return nodors;
+  }
 }
+
+export const hodor = (input: string) => {
+  const interpreter = Environment.createStandard().createInterpreter();
+  return interpreter.execute(input);
+};
 
 export default Hodor;
